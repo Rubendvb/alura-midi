7f1882d98f39d468825b527da4840f4a4d35db48
Rubendvb <rubenvasquez556@gmail.com>
2022-06-14 19:10:25 -0300

    Finalizando projeto

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
 function tocaSom(idElemento) {
-  document.querySelector(idElemento).play();
+  const elemento = document.querySelector(idElemento);
+  elemento.play();
 }
 
 const listaTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
-
-while (contador < listaTeclas.length) {
-  const tecla = listaTeclas[contador];
+for (let i = 0; i < listaTeclas.length; i++) {
+  const tecla = listaTeclas[i];
   let instrumento = tecla.classList[1];
 
   const idAudio = `#som_${instrumento}`;
 
-  listaTeclas[contador].addEventListener("click", function () {
+  listaTeclas[i].addEventListener("click", function () {
     tocaSom(idAudio);
   });
-  contador++;
+
+  listaTeclas[i].addEventListener("keydown", function (e) {
+    if (e.keyCode === 13 || e.keyCode == 32) {
+      listaTeclas[i].classList.add("ativa");
+    }
+  });
+
+  listaTeclas[i].addEventListener("keyup", function (e) {
+    if (e.keyCode === 13 || e.keyCode == 32) {
+      listaTeclas[i].classList.remove("ativa");
+    }
+  });
 }
